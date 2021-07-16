1fa8b2eaf7149d93819473b8d4e747844fd1487c
Marlos <marlostrinidad@gmail.com>
2021-07-15 21:35:02 -0300

    fazendo a tela login

---
+++ android/app/src/main/java/com/MarlosTrinidad/fogaca_app/model/MotoboyAccount.java
@@ -0,0 +1,45 @@
+package com.MarlosTrinidad.fogaca_app.model;
+
+public class MotoboyAccount {
+    String id;
+    String nome;
+    String email;
+    String telefone;
+    String icon_foto;
+    String modelo;
+    String placa;
+    String cor;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getIcon_foto() {
+        return icon_foto;
+    }
+}
